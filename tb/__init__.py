de138bd224bf6f000c21e0f279e6206ddfa650b5
Alex Hagen <alexander.hagen@pnnl.gov>
2021-01-21 15:01:34 -0800

    added alert and got the timer working

---
+++ tb/__init__.py
@@ -0,0 +1,35 @@
+import rumps
+import datetime
+
+class TimeBlocker:
+    def __init__(self):
+        self.config = dict(app_name='TimeBlocker')
+        self.app = rumps.App(self.config['app_name'])
+        self.timer = rumps.Timer(self.on_tick, 5)
+        self.set_up_menu()
+        #buttons
+        #self.app.menu = [buttons]
+
+    def on_tick(self, sender):
+        now = datetime.datetime.now()
+        if now.weekday() <= 5:
+            if ((now.minute >= 25 and now.minute <= 29) or 
+                (now.minute >= 55 and now.minute <= 59)):
+                    if self.app.title == 'work':
+                        rumps.alert("Break", "Time to take a break", "ok", "cancel")
+                    self.app.title = 'break'
+            else:
+                if self.app.title == 'break':
+                    rumps.alert("Work", "Time to work", "ok", "cancel")
+                self.app.title = 'work'
+
+    def set_up_menu(self):
+        self.app.title = "work"
+        self.timer.start()
+
+    def run(self):
+        self.app.run()
+
+if __name__ == "__main__":
+    tb = TimeBlocker()
+    tb.run()
