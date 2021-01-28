e84b43050c2363c8b2ee0c2164b8e77012ae80d2
Alex Hagen <alexander.hagen@pnnl.gov>
2021-01-28 14:51:38 -0800

    install with timeblocker name working

--- tb/__init__.py
+++ tb/__init__.py
@@ -9,9 +9,24 @@ class TimeBlocker:
         self.config = dict(app_name='TimeBlocker')
         self.app = rumps.App(self.config['app_name'])
         self.timer = rumps.Timer(self.on_tick, 5)
+        self.config = dict(start='pause')
+        self.pause = False
+        self.start_pause_button = rumps.MenuItem(title='pause', callback=self.toggle_pause)
         self.set_up_menu()
+        self.app.menu = [self.start_pause_button]
         #buttons
         #self.app.menu = [buttons]
+
+    def toggle_pause(self, sender):
+        """Toggle the menu from start to paused on click."""
+        if self.pause:
+            self.app.title = 'checking'
+            sender.title = 'pause'
+            self.pause = False
+        else:
+            self.app.title = 'paused'
+            sender.title = 'start'
+            self.pause = True
 
     def on_tick(self, _):
         """Check minute and perform actions if a break."""
@@ -21,7 +36,7 @@ class TimeBlocker:
         is_top_of_the_hour = (now.minute >= 25 and now.minute <= 29)
         is_bottom_of_the_hour = (now.minute >= 55 and now.minute <= 59)
         is_break = is_top_of_the_hour or is_bottom_of_the_hour
-        if is_weekday and is_workhour:
+        if is_weekday and is_workhour and not self.pause:
             if is_break:
                 if self.app.title == 'work':
                     rumps.notification("Break", "Time to take a break", "ok")
